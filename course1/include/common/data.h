2b3213658cb6d677f8b1ba89c8a3d07f2ff31f76
Sudakov Andrejj <andrejj_vs@mail.ru>
2018-05-17 05:37:34 -0600

    Added covertion functions prototypes and descriptions

--- course1/include/common/data.h
+++ course1/include/common/data.h
@@ -8,8 +8,47 @@
  * misuse of this material. 
  *
  *****************************************************************************/
+/**
+ * @file data.h
+ * @brief Type convertion functions
+ *
+ * This header file provides an convertion from a standard integer type into
+ * an ASCII string and vice versa.
+ *
+ * @author Andrei Sudakov
+ * @date May 17 2018
+ *
+ */
 #ifndef __DATA_H__
 #define __DATA_H__
+
+/**
+ * @brief Convert data from a standard integer type into an ASCII string. 
+ *
+ * Takes an integer number, a base for convertaion and allocation for 
+ * result to convert input integer to ASCII string.
+ *
+ * @param data 32-bit signed number
+ * @param ptr Pointer on allocation of result string
+ * @param base base (2 to 16)
+ *
+ * @return uint8_t.
+ */
+uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base);
+
+/**
+ * @brief Convert data from a ASCII string into an standard integer type. 
+ *
+ * Takes an allocation of ASCII string, a number of digits and base of 
+ * represented number to convert input ASCII string to integer.
+ *
+ * @param ptr Pointer on allocation of ASCII string
+ * @param digits number of digits in input string
+ * @param base base (2 to 16)
+ *
+ * @return int32_t.
+ */
+int32_t my_atoi(uint8_t * ptr, uint8_t digits, uint32_t base);
 
 #endif /* __DATA_H__ */
 
