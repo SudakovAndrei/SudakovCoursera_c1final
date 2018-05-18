4e8f8bd395285e3feb302a0ca714dcf92b81e65d
Sudakov Andrejj <andrejj_vs@mail.ru>
2018-05-18 04:39:42 -0600

    Added test functions prototypes and some descriptions

--- course1/include/common/course1.h
+++ course1/include/common/course1.h
@@ -8,8 +8,101 @@
  * misuse of this material. 
  *
  *****************************************************************************/
+/**
+ * @file course1.h
+ * @brief Type convertion functions
+ *
+ * This header file provides an test function. 
+ *
+ * @author Andrei Sudakov
+ * @date May 17 2018
+ *
+ */
 #ifndef __COURSE1_H__
 #define __COURSE1_H__
+
+/**
+ * @brief Call a handful of functions and test function outputs. 
+ *
+ * Call each of the following functions once. 
+ *
+ * @return uint8_t.
+ */
+int8_t course1(void);
+
+/**
+ * @brief Test atoi and itoa conversions functions. 
+ *
+ *  
+ *
+ * @return uint8_t.
+ */
+int8_t test_data1(void);
+
+/**
+ * @brief Test atoi and itoa conversions functions. 
+ *
+ *  
+ *
+ * @return uint8_t.
+ */
+int8_t test_data2(void);
+
+/**
+ * @brief Test non-overlapped memmove function. 
+ *
+ *
+ *
+ * @return uint8_t.
+ */
+int8_t test_memmove1(void);
+
+/**
+ * @brief Test overlapped memmove function. 
+ *
+ * In this test overlap overlap start of destination and end
+ * of the source regions. 
+ *
+ * @return uint8_t.
+ */
+int8_t test_memmove2(void);
+
+/**
+ * @brief Test overlapped memmove function. 
+ *
+ * In this test overlap end of destination and start of the 
+ * source regions.
+ *
+ * @return uint8_t.
+ */
+int8_t test_memmove3(void);
+
+/**
+ * @brief Test memcopy function. 
+ *
+ *
+ *
+ * @return uint8_t.
+ */
+int8_t test_memcopy(void);
+
+/**
+ * @brief Test memset function. 
+ *
+ *
+ *
+ * @return uint8_t.
+ */
+int8_t test_memset(void);
+
+/**
+ * @brief Test reverse function. 
+ *
+ *
+ *
+ * @return uint8_t.
+ */
+int8_t test_reverse(void);
 
 #endif /* __COURSE1_H__ */
 
